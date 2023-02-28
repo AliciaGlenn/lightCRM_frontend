32927841a537c8cfcdd9f4a8c7e087db009ab7d0
AliciaGlenn <Alicia@AliciaGlenn.com>
2023-02-28 16:17:59 -0500

    update and create action fixes

--- src/router/actions.js
+++ src/router/actions.js
@@ -1,9 +1,34 @@
 import { redirect } from "react-router-dom";
 import URL from "./baseurl";
 
+// export const createAction = async ({ request }) => {
+//   const formData = await request.formData();
+
+//   const newContact = {
+//     companyName: formData.get("companyName"),
+//     person: formData.get("person"),
+//     personTitle: formData.get("personTitle"),
+//     emailAddress: formData.get("emailAddress"),
+//     usState: formData.get("usState"),
+//   };
+
+//   await fetch(URL, {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(newContact),
+//   });
+
+//   return redirect("/");
+// };
+
+//createAction => create a todo from form submissions to `/create`
 export const createAction = async ({ request }) => {
+  // get form data
   const formData = await request.formData();
 
+  // construct request body
   const newContact = {
     companyName: formData.get("companyName"),
     person: formData.get("person"),
@@ -12,14 +37,16 @@ export const createAction = async ({ request }) => {
     usState: formData.get("usState"),
   };
 
-  await fetch(URL, {
-    method: "POST",
+  // send request to backend
+  await fetch(URL + "/contacts", {
+    method: "post",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(newContact),
   });
 
+  // redirect back to index page
   return redirect("/");
 };
 
